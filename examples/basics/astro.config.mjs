c5b65c7cb17b0406935f1fb95eb797b9b72cb254
xiaoxi303 <zengjunjie066@gmail.com>
2026-03-25 20:57:37 +0800

    Update astro.config.mjs

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -6,18 +6,29 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: '个人主页', // 👈 这里改成你的名字
+			defaultLocale: 'zh-cn', // 设置默认语言为中文
+			locales: {
+				'zh-cn': {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
+			social: {
+				// 👈 把下面的 href 换成你自己的 GitHub 主页链接
+				github: 'https://github.com/xiaoxi303', 
+			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '关于我',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						// 这里对应 src/content/docs/guides/example.md 文件的内容
+						{ label: '个人简介', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: '我的项目',
+					// 自动读取 src/content/docs/reference/ 文件夹下的所有 md 文件
 					autogenerate: { directory: 'reference' },
 				},
 			],
